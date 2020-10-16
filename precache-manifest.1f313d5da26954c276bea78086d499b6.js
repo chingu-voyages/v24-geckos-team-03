9b1e7f3e52f8f509dce52da7a31fba7f5db12988
dpajek <dan.pajek@gmail.com>
2020-10-16 18:48:44 -0400

    Updates

---
+++ precache-manifest.1f313d5da26954c276bea78086d499b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804bbd7dfd6193200a8ce98bc554e154",
+    "url": "/v24-geckos-team-03/index.html"
+  },
+  {
+    "revision": "6fb9bab8325ca9d9e16e",
+    "url": "/v24-geckos-team-03/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0117c771c6477a5ee4e6",
+    "url": "/v24-geckos-team-03/static/js/2.daa624ad.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/v24-geckos-team-03/static/js/2.daa624ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fb9bab8325ca9d9e16e",
+    "url": "/v24-geckos-team-03/static/js/main.586ba1fc.chunk.js"
+  },
+  {
+    "revision": "133ad083b138c7f84375",
+    "url": "/v24-geckos-team-03/static/js/runtime-main.b3cf23ef.js"
+  },
+  {
+    "revision": "c9888dc100141b85dd116cf238ce98b6",
+    "url": "/v24-geckos-team-03/static/media/not-found.c9888dc1.jpg"
+  }
+]);
