7f21e6ab87d4a60f3ee2d3251c9f9659c9382ef1
Erion Pali <eerian00@gmail.com>
2020-10-15 23:49:18 -0400

    Added Chakra-ui / Created a Card
    -Created a few cards experimenting with Chakra-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider, Box, Image, Heading, Text, Stack} from '@chakra-ui/core';
 
 const App = () => {
-  return <div className="red">MOVIE DATABASE</div>;
+  return (
+    <ThemeProvider>
+      <Heading textAlign='center'>Movie Database</Heading>
+      <Stack isInline>
+        {/* Create boxes */}
+        <Box w='150px' border='1px solid red' overflow='hidden' rounded='10px' boxShadow='sm'>
+          <Image src={require('../img/not-found.jpg')} alt='pic' />
+          <Box>
+            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
+            <Text fontSize='50%'>Description</Text>
+          </Box>
+        </Box>
+        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
+          <Image src={require('../img/not-found.jpg')} alt='pic' />
+          <Box>
+            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
+            <Text fontSize='50%'>Description</Text>
+          </Box>
+        </Box>
+        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
+          <Image src={require('../img/not-found.jpg')} alt='pic' />
+          <Box>
+            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
+            <Text fontSize='50%'>Description</Text>
+          </Box>
+        </Box>
+        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
+          <Image src={require('../img/not-found.jpg')} alt='pic' />
+          <Box>
+            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
+            <Text fontSize='50%'>Description</Text>
+          </Box>
+        </Box>
+        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
+          <Image src={require('../img/not-found.jpg')} alt='pic' />
+          <Box>
+            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
+            <Text fontSize='50%'>Description</Text>
+          </Box>
+        </Box>
+   
+      </Stack>
+      
+     
+    </ThemeProvider>
+   
+  )
 };
 
 export default App;
