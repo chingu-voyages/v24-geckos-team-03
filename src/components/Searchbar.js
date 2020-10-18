d8614152249198ae9c7e00b76d77e4aeb1c519ee
Rayhan1998 <miahrayhan01@gmail.com>
2020-10-18 19:55:32 -0400

    feat: set up input search state

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import axios from "axios";
 
 function Search() {
+  const [search, setSearch] = useState("");
+  const inputEl = useRef(null);
+
+  function searchSubmit(e) {
+    e.preventDefault();
+    setSearch(inputEl.current.value);
+  }
+
   return (
     <div>
-      <form></form>
+      <form onSubmit={searchSubmit}>
+        <input type="text" ref={inputEl}></input>
+        <button>Search Movies</button>
+      </form>
     </div>
   );
 }
