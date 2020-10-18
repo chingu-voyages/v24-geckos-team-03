389773599c04e88b70266e5e30f140f7d11830d2
Rayhan1998 <miahrayhan01@gmail.com>
2020-10-18 19:42:45 -0400

    created example search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,75 @@
 import React from "react";
-import { ThemeProvider, Box, Image, Heading, Text, Stack} from '@chakra-ui/core';
+import Searchbar from "./Searchbar";
+import {
+  ThemeProvider,
+  Box,
+  Image,
+  Heading,
+  Text,
+  Stack
+} from "@chakra-ui/core";
 
 const App = () => {
   return (
     <ThemeProvider>
-      <Heading textAlign='center'>Movie Database</Heading>
+      <Heading textAlign="center">Movie Database</Heading>
+      <Searchbar />
       <Stack isInline>
         {/* Create boxes */}
-        <Box w='150px' border='1px solid red' overflow='hidden' rounded='10px' boxShadow='sm'>
-          <Image src={require('../img/not-found.jpg')} alt='pic' />
+        <Box
+          w="150px"
+          border="1px solid red"
+          overflow="hidden"
+          rounded="10px"
+          boxShadow="sm"
+        >
+          <Image src={require("../img/not-found.jpg")} alt="pic" />
           <Box>
-            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
-            <Text fontSize='50%'>Description</Text>
+            <Text as="h5" mt="-3px" mb="-5px">
+              Movie Title
+            </Text>
+            <Text fontSize="50%">Description</Text>
           </Box>
         </Box>
-        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
-          <Image src={require('../img/not-found.jpg')} alt='pic' />
+        <Box w="150px" border="1px solid red" overflow="hidden" boxShadow="sm">
+          <Image src={require("../img/not-found.jpg")} alt="pic" />
           <Box>
-            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
-            <Text fontSize='50%'>Description</Text>
+            <Text as="h5" mt="-3px" mb="-5px">
+              Movie Title
+            </Text>
+            <Text fontSize="50%">Description</Text>
           </Box>
         </Box>
-        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
-          <Image src={require('../img/not-found.jpg')} alt='pic' />
+        <Box w="150px" border="1px solid red" overflow="hidden" boxShadow="sm">
+          <Image src={require("../img/not-found.jpg")} alt="pic" />
           <Box>
-            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
-            <Text fontSize='50%'>Description</Text>
+            <Text as="h5" mt="-3px" mb="-5px">
+              Movie Title
+            </Text>
+            <Text fontSize="50%">Description</Text>
           </Box>
         </Box>
-        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
-          <Image src={require('../img/not-found.jpg')} alt='pic' />
+        <Box w="150px" border="1px solid red" overflow="hidden" boxShadow="sm">
+          <Image src={require("../img/not-found.jpg")} alt="pic" />
           <Box>
-            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
-            <Text fontSize='50%'>Description</Text>
+            <Text as="h5" mt="-3px" mb="-5px">
+              Movie Title
+            </Text>
+            <Text fontSize="50%">Description</Text>
           </Box>
         </Box>
-        <Box w='150px' border='1px solid red' overflow='hidden' boxShadow='sm'>
-          <Image src={require('../img/not-found.jpg')} alt='pic' />
+        <Box w="150px" border="1px solid red" overflow="hidden" boxShadow="sm">
+          <Image src={require("../img/not-found.jpg")} alt="pic" />
           <Box>
-            <Text as='h5' mt='-3px' mb='-5px'>Movie Title</Text>
-            <Text fontSize='50%'>Description</Text>
+            <Text as="h5" mt="-3px" mb="-5px">
+              Movie Title
+            </Text>
+            <Text fontSize="50%">Description</Text>
           </Box>
         </Box>
-   
       </Stack>
-      
-     
     </ThemeProvider>
-   
-  )
+  );
 };
 
 export default App;
---
+++ src/components/Searchbar.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+function Search() {
+  return <div>Hello</div>;
+}
+
+export default Search;
