9777b6e98618d3533d82650f4363323d994da8a6
dpajek <dan.pajek@gmail.com>
2020-10-23 21:43:52 -0400

    Added Chakra ThemeProvider

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ContextProvider } from "./Context";
 import "./index.css";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 
 ReactDOM.render(
   <ContextProvider>
     <Router>
-      <App />
+      <ThemeProvider>
+        <CSSReset />
+        <App />
+      </ThemeProvider>
     </Router>
   </ContextProvider>,
   document.querySelector("#root")
