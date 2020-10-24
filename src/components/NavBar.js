fc95490cbbb83fe076d5cc9d473172749663572f
Erion Pali <eerian00@gmail.com>
2020-10-23 23:51:58 -0400

    Added some fonts for fun, aligned navbar items better

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,40 @@
 import React from "react";
 import Searchbar from './Searchbar';
-import { ThemeProvider, Box, Flex, Link, Image,  Text , Heading, Stack} from "@chakra-ui/core";
+import { ThemeProvider, Box, Flex, Link, Image,  Switch , Heading, Stack} from "@chakra-ui/core";
 
 const NavBar = () => {
     return (
         <ThemeProvider>
-                <Flex
-        bg="#333"
-        w="100%"
-        px={5}
-        py={4}
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Flex flexDirection="row" justifyContent="center" alignItems="center">
-          <Image
-            src="https://cdn1.iconfinder.com/data/icons/media-colorful-1/48/film_roll-512.png"
-            size={30}
-          />
-          <Heading as="h2" size="md" pl={3} color="#49c3fd">
-            Daily Flix
-          </Heading>
-        </Flex>
-        
-        <Box mr={20}>
-        <Searchbar />
-          <Link px={2} color="#fff" textDecoration="none">My Favorites</Link>
-          <Link px={2} color="#fff">Watched</Link>
-          <Link px={2} color="#fff">Watch List</Link>
+            <Box className="nav-bar"
+              bg="#333"
+              w="100%"
+              px={5}
+              py={2}
+            >
           
-        </Box>
-      </Flex>
+            <Stack isInline justifyContent="space-between">
+              <Stack isInline>
+                <Image
+                  src="https://cdn1.iconfinder.com/data/icons/media-colorful-1/48/film_roll-512.png"
+                  size={30}
+                />
+                <Heading as="h3" size="lg" color="#49c3fd">
+                  Daily Flix
+                </Heading>
+              </Stack>
+              <Stack isInline>
+                <Searchbar />
+                <Switch size="sm" ml={2}/>
+              </Stack>
+            </Stack>
+              
+            <Box float="right" mr={10}>
+              <Link px={2} color="#fff" className="link">My Favorites</Link>
+              <Link px={2} color="#fff" className="link">Watched</Link>
+              <Link px={2} color="#fff" className="link">Watch List</Link>
+            </Box>
+          </Box>
+      
         </ThemeProvider>
     )
 }
