01c1151ffdec95394e3075de2bae3d5bf4651baf
Rayhan1998 <miahrayhan01@gmail.com>
2020-10-25 16:41:59 -0400

    bug: fixed homepage bugs

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,18 +1,10 @@
-import React, { useContext } from "react";
-import Searchbar from "../components/Searchbar";
+import React from "react";
+
 import Grid from "../components/Grid";
-import { Context } from "../Context";
+
 import NavBar from "../components/NavBar";
 
 function Homepage() {
-  const {
-    setSearch,
-    setSearchResults,
-    search,
-    searchResults,
-    setIsSearch,
-    isSearch
-  } = useContext(Context);
   return (
     <div
       style={{
