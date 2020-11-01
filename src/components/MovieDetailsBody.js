5f4850731161447a3c21601a3c2145765ab528ac
Rayhan1998 <miahrayhan01@gmail.com>
2020-11-01 16:47:28 -0500

    feat: added cast name and headshots to movie details page

--- src/components/MovieDetailsBody.js
+++ src/components/MovieDetailsBody.js
@@ -1,13 +1,60 @@
 import React, { useEffect, useContext, useState } from "react";
 import axios from "axios";
 import { Context } from "../Context";
-import { Box, Heading, Image } from "@chakra-ui/core";
+import { Box, Heading, Image, Text, Flex } from "@chakra-ui/core";
+import { Link } from "react-router-dom";
+import { position } from "styled-system";
 
 function MovieDetailsBody(props) {
-  const { APIKEY } = useContext(Context);
-  const [movieData, setMoviedata] = useState([]);
-  const [moviecast, setMovieCast] = useState([]);
+  const { APIKEY, ImageUrl, setPersonId, setDefaultMovies } = useContext(
+    Context
+  );
 
+  const [movieData, setMovieData] = useState([]);
+  const [movieCast, setMovieCast] = useState([]);
+
+  function searchByActor(person_id) {
+    setPersonId(person_id);
+  }
+  let castList = []; // Holds all the movie cast headshots/name/
+  if (movieCast !== null) {
+    const numberOfActorsDisplayed = 10;
+    // the code beloe takes the first X objects in the movie credit array
+    // for each of these, a Box is generated with a headshot, actor name, and character
+    castList = movieCast
+      .slice(0, numberOfActorsDisplayed)
+      .map((castMember, index) => {
+        const {
+          cast_id,
+          character,
+          name,
+          profile_path,
+          id: person_id
+        } = castMember;
+
+        return (
+          <Box key={cast_id} textAlign="center">
+            <Image
+              m="0 auto"
+              // onClick={() => searchByActor(person_id)} Will implement in next week MVP
+              cursor="pointer"
+              rounded="lg"
+              src={ImageUrl + profile_path}
+              h="80px"
+              objectFit="cover"
+            />
+            <Box p="7px">
+              {name} <br />{" "}
+              <Text fontSize="0.9em" fontStyle="italic">
+                {character}
+              </Text>
+            </Box>
+          </Box>
+        );
+      });
+  }
+
+  // Id is passed from MovieDetails component
   const { movieId } = props;
 
   useEffect(() => {
@@ -15,7 +62,8 @@ function MovieDetailsBody(props) {
       axios
         .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${APIKEY}`)
         .then(res => {
-          setMoviedata(res.data);
+          // stores movie data using movie ID
+          setMovieData(res.data);
         });
     } catch (err) {
       console.log(err);
@@ -27,6 +75,7 @@ function MovieDetailsBody(props) {
           `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${APIKEY}`
         )
         .then(res => {
+          // stores cast data using movie ID
           setMovieCast(res.data.cast);
         });
     } catch (err) {
@@ -34,6 +83,7 @@ function MovieDetailsBody(props) {
     }
   }, []);
   console.log(movieData);
+  console.log(movieCast);
 
   const detailBody = {
     backgroundColor: "#333333",
@@ -45,15 +95,52 @@ function MovieDetailsBody(props) {
     height: "100vh",
     width: "100vw",
     backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.backdrop_path})`,
-    backgroundSize: "cover"
+    backgroundSize: "cover",
+    position: "relative"
+  };
+
+  const container = {
+    maxWidth: "80%",
+    margin: "0 auto",
+    color: "white"
   };
   return (
     <>
       <div style={imageContainer}>
-        <Heading color="white">{movieData.original_title}</Heading>
+        <div
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.5)"
+          }}
+        >
+          <div style={container}>
+            <Heading color="white">{movieData.original_title}</Heading>
+            <Link to="/">
+              <span style={{ color: "white", fontSize: "80px" }}>&#8592;</span>
+            </Link>
+          </div>
+        </div>
       </div>
       <div style={detailBody}>
-        <h1>Hello</h1>
+        <div style={container}>
+          <Box>
+            <Heading>Summary</Heading>
+            <p>{movieData.overview}</p>
+          </Box>
+
+          <Box>
+            <Heading>Cast</Heading>
+            <Flex>{castList}</Flex>
+          </Box>
+
+          <Box>
+            <Heading>Trailer</Heading>
+          </Box>
+        </div>
       </div>
     </>
   );
