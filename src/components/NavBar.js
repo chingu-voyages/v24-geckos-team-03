aaf8dbdbba6ad6847014a283d4cedef567a259a7
Erion Pali <eerian00@gmail.com>
2020-11-01 10:39:02 -0500

    adjusted navbar and added a border

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,26 @@ const NavBar = () => {
     console.log(defaultMovies);
     console.log("hello");
   };
+
+  const stickyNav = {
+    position : "fixed",
+    boxShadow : "0 2px 2px #49c3fd",
+    zIndex : "9999"
+
+  }
+
+
+
   return (
-    <Box className="nav-bar" bg="primaryBackground" w="100%" px={5} py={2}>
+         <Box bg="primaryBackground" 
+          w="100%" 
+          px={5} 
+          py={5}
+          paddingBottom="10px" 
+          style={stickyNav}
+
+          >
+
       <Stack isInline justifyContent="space-between">
         <Link as={RouterLink} to="/">
           <Stack isInline onClick={clicked}>
@@ -21,8 +39,8 @@ const NavBar = () => {
               src="https://cdn1.iconfinder.com/data/icons/media-colorful-1/48/film_roll-512.png"
               size={46}
             />
-            <Heading as="h3" size="xl" color="#49c3fd" fontFamily="Nunito">
-              Daily Flix
+            <Heading as="h3" size="xl" fontWeight="200" color="#49c3fd" fontFamily="Gugi, cursive">
+              DAILY FLIX
             </Heading>
           </Stack>
           </Link>
@@ -31,12 +49,14 @@ const NavBar = () => {
             <Switch size="sm" ml={2}/>
           </Stack>
         </Stack>
-        <Box float="right" marginRight="198px" pt="4px">
+        <Box float="right" marginRight="198px">
             {/* Use RouterLink as written in ChakraUI docs */}
             <Link textDecoration="none" _hover={{color : "#49c3fd"}} as={RouterLink} to="/favorites" px={2} color="#fff" className="link">My Favorites</Link>
             <Link _hover={{color : "#49c3fd"}} className="navbar-links" px={2} color="#fff" className="link">Watch List</Link>
         </Box>
       </Box>
+
+ 
       
     )
 }
